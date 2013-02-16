732f4cada2f8ff571828cd2cabd4807bed2ca835
Travis Corrigan <travis.corrigan@gmail.com>
2013-02-16 01:04:25 -0800

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,9 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+y = 36
+pets = ['rex', 'nibbles', 'fred']
+template = ERB.new "The value of x is: <%= x %> and the the value of y is: <%= y %>"
+addition = ERB.new "The value of x and y together is <%= x + y %>!!"
 puts template.result(binding)
+puts addition.result(binding)
